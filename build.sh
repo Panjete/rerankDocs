04f563b53b3abed28de5409fff7f2022090f6d22
Panjete <suparmaansingh@gmail.com>
2023-10-20 17:44:38 +0530

    final working version

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ make
 cd ..
 
 gcc word2vec.c -o word2vec -lm -pthread -O3 -march=native -Wall -funroll-loops -Wno-unused-result
-gcc distance.c -o distance -lm -pthread -O3 -march=native -Wall -funroll-loops -Wno-unused-result
+# gcc distance.c -o distance -lm -pthread -O3 -march=native -Wall -funroll-loops -Wno-unused-result
--- getndcg.sh
+++ getndcg.sh
@@ -5,8 +5,11 @@ rm trec_eval-9.0.7/out_w2v.txt
 cp out.txt trec_eval-9.0.7/out.txt
 cp out_w2v.txt trec_eval-9.0.7/out_w2v.txt
 cd trec_eval-9.0.7
+make
 echo "rm1 accuracies\n"
 ./trec_eval -m ndcg -m ndcg_cut.5,10,50 t40-qrels.txt out.txt
 echo "w2v accuracies\n"
 ./trec_eval -m ndcg -m ndcg_cut.5,10,50 t40-qrels.txt out_w2v.txt
 cd ..
+
+#replace out.txt and out_w2v.txt if reusing this. Was used for streamlined evaluation while hyper-param tuning
